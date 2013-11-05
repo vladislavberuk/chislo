e812799dede5712c3aafa648ca0b0722b5179383
vlad <vladislav_v2009@mail.ru>
2013-11-05 20:32:39 +0700

    изменение

--- src/GuessNumber.java
+++ src/GuessNumber.java
@@ -1,25 +1,10 @@
-import java.util.Random;
-import java.util.Scanner;
 
 
 
 public class GuessNumber {
     public static void main(String[] args) {
         GuessLogic guessLogic = new  GuessLogic();
-        GuessInterface userInterface = new GuessInterface();
 
-        // 1. Научиться получить случайное число
-        // Ctrl - Q
-
-
-
-        // 2. Попросить у пользователя число и
-        // прочитать с консоли
-
-
-        // 3. Проверить число и указать больше оно или меньше нужного
-        // if( ... ) { /* выдать сообщение о победе */}
-        //    else { /*выдать сообщение о больше/меньше */ } `
 
         int secret = guessLogic.generateSecretNumber();
 
@@ -28,11 +13,18 @@ public class GuessNumber {
         guessLogic.work();
 
 
+        // 1. Научиться получить случайное число
+        // Ctrl - Q
+
+        // 2. Попросить у пользователя число и
+        // прочитать с консоли
+
+        // 3. Проверить число и указать больше оно или меньше нужного
+        // if( ... ) { /* выдать сообщение о победе */}
+        //    else { /*выдать сообщение о больше/меньше */ } `
+
         // 4. Научиться повторять действия 2-3 в цикле
         //    while(...) {/* делаем действия 2 и 3*/}
-
-
-
 
         // 5. Когда цикл закончился - выдать правильный ответ
         //    System.out.println
